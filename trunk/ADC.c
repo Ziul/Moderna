17fd44459346d7f3d56843a46fbbb7721ad05aeb
Lucas <lucassalves65@gmail.com>
2012-12-06 14:10:08 +0000

    add do adc.c e .h

---
+++ trunk/ADC.c
@@ -0,0 +1,23 @@
+#include "ADC.h"
+#include "leagacymsp430.h"
+#include "disp.h"
+
+
+void init_adc(){
+
+	WDTCTL = WDTPW + WDTHOLD;                	// Stop WDT
+	ADC10CTL1 = CONSEQ_2 + INCH_1;          	// Repeat single channel, A1
+	ADC10CTL0 = ADC10SHT_2 + MSC + ADC10ON + ADC10IE; // ADC10ON, interrupt enable
+	ADC10DTC1 = 0;								// no reads yet
+	ADC10AE0 |= 0x02; 
+
+}
+
+int read_adc(){
+	ADC10DTC1 = 1;							// one read
+	ADC10CTL0 &= ~ENC;
+	while (ADC10CTL1 & BUSY);               // Wait if ADC10 core is active
+	ADC10SA = 0x200;                        // Data buffer address
+	ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+	return ADC10MEM;
+}
---
+++ trunk/ADC.h
@@ -0,0 +1,9 @@
+#ifndef __ADC_H__
+#define __ADC_H__
+
+extern
+void init_adc();
+extern
+int read_adc();
+
+#endif
