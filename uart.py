8a8efa0c6bfcd14f8cd6b4d50c8ee856ce46d2ed
Luiz Oliveira <ziuloliveira@gmail.com>
2012-12-12 03:27:57 +0000

    Agora acho que não tem que mecher mais neste aqui viu!

--- uart.py
+++ uart.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
 import serial		# metodos para a serial
 import os			# metodos para SO
+import subprocess	# para o gnuplot
+import sys			# para ter acesso aos parametros
 
 """
 import signal		# pegando sinais como em C
@@ -9,30 +12,30 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
+#port = raw_input("Entre com a operacao desejada: ")
 """
 
-
-f=file('./saida.txt','w')
-
-print "Inicializando serial..."
-ser = serial.Serial('/dev/ttyACM1',9600)
 try:
+	ser = serial.Serial(sys.argv[1],9600)
 	ser.open()
-except SerialException:
-	print "Nao consegui abrir a porta!"
+except IndexError:
+	print "Passe uma porta existente com o parametro!"
+	print "\teg.: python " + sys.argv[0] + " /dev/ttyUSB0"
+	print "\teg.: ./" + sys.argv[0] + " /dev/ttyUSB0"
+	sys.exit(0)
 
-print "Aberta?"
-print ser.isOpen()
-print "Legivel?" 
-print ser.readable()
-ser.flush()
-#limpa tela
+if ser.isOpen() & ser.readable():
+	f=file('./saida.txt','w')
+else:
+	print "Porta nao acessivel"
+	sys.exit(0)
+
+#limpa tela e buffer
 os.system('clear')
 ser.flushInput()
 print "Aperte Ctrl+C para terminar a leitura\n\n"
 try:
 	while True:
-#		msg = ser.readline()
 		msg = ser.read()
 		print msg 
 		f.write(msg)
@@ -45,4 +48,13 @@ f.write('\n')
 
 print "Fechando arquivo \"saida.txt\""
 f.close()
-print "Serial fechada"
+print "Serial fechada. Abrindo o GNUPLOT"
+
+proc = subprocess.Popen(['gnuplot','-p'], 
+                        shell=True,
+                        stdin=subprocess.PIPE,
+                        )
+                        
+proc.stdin.write("plot 'saida.txt' with lines\n")
+proc.stdin.write("pause mouse\n")
+proc.stdin.write("quit\n")
