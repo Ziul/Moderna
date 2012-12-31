4f80b3374ba05205dcd5dfcc21fdc275559344c5
Luiz Oliveira <ziuloliveira@gmail.com>
2012-12-31 10:05:14 +0000

    # Python * Verificação da existencia da porta mais efetiva * Tratamento de erros nas capturas * Uso da biblioteca Curses na saída * Melhor controle do GNUPLOT
    # LaTeX
    * Revisão do programa Python
    * Inclusão de uma parte inutil mas que gostaria de saber como sairia a inclusão de trechos em Japones no documento. TODO: Remover em breve o conteudo

--- uart.py
+++ uart.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
+# -*- coding:utf-8 -*-
+
 import serial		# metodos para a serial
 import os			# metodos para SO
 import subprocess	# para subprocessos (gnuplot)
 import sys			# para ter acesso aos parametros
-
-def floatraw( aString ):
-    return eval( aString )
+import curses		# para a biblioteca curses
 
 """
 import signal		# pegando sinais como em C
@@ -15,7 +15,6 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
-#port = raw_input("Entre com a operacao desejada: ")
 """
 
 try:
@@ -26,51 +25,94 @@ except IndexError:
 	print "\teg.: python " + sys.argv[0] + " /dev/ttyUSB0"
 	print "\teg.: ./" + sys.argv[0] + " /dev/ttyUSB0"
 	sys.exit(0)
+except IOError as e:
+    print("({})".format(e))
+    sys.exit(0)
+    raise
 
 if ser.isOpen() & ser.readable():
 	f=file('./saida.txt','w')
-	fx=file('./trabalhada.tex','w')
+	fx=file('./trabalhada.dot','w')
 	fx.write("(0,0) " )
+	print "Porta aberta!"
 
 else:
-	print "Porta nao acessivel"
+	print "Porta não acessível"
 	sys.exit(0)
 
 #limpa tela e buffer
-os.system('clear')
+#os.system('clear')
 ser.flushInput()
-print "Aperte Ctrl+C para terminar a leitura\n\n"
+
+stdscr = curses.initscr()
+curses.noecho()
+
+#print "Aperte Ctrl+C para terminar a leitura\n\n"
+stdscr.addstr (0,0,"Aperte Ctrl+C para terminar a leitura\n\n",curses.A_BOLD)
 while True:
 	try:
 		msg = ser.readline()
-		print msg 
+		#print  msg,
+		stdscr.addstr (3,0,msg,curses.A_REVERSE)
+		stdscr.refresh()
 	except KeyboardInterrupt:
-		print "\rFim da leitura!"
 		break	
+	
 	finally:
-		f.write(msg)
-		vsel = eval(msg.split(" ")[0])/10.0
-		fx.write("-- (%.2f,%.2f) " % (vsel,  eval(msg.split(" ")[1])/100.0  ) )
+		try:
+			f.write(msg)
+			vsel = eval(msg.split(" ")[0])/10.0
+			fx.write("-- (%.2f,%.2f) " % (vsel,  eval(msg.split(" ")[1])/100.0  ) )
+		except:
+			curses.nocbreak(); stdscr.keypad(0); curses.echo()
+			curses.endwin()
+			try:
+				if len(msg) > 1:
+					print "\rFalha na sincronização!\nFoi capturado contudo inválido \n" + msg 
+			except:
+				print "\rNão foi capturado nada!"
+			ser.close()
+			f.close()
+			fx.close()
+			sys.exit(0)
 
+
+stdscr.addstr (5,0, "\r   \nFim da leitura!")
 ser.close()
 
-#f.write('\n')
-
-print "Fechando arquivo \"saida.txt\""
+stdscr.addstr (6,0, "Fechando arquivo \"saida.txt\"")
 f.close()
 fx.close()
-print "Serial fechada. Abrindo o GNUPLOT"
+stdscr.addstr (7,0, "Serial fechada. Abrindo o GNUPLOT")
+stdscr.refresh()
 
 proc = subprocess.Popen(['gnuplot','-p'], 
                         shell=True,
                         stdin=subprocess.PIPE,
                         )
                         
+#Margem segura para visualizar os dados
+proc.stdin.write("set yrange [-1:1300]\n")
 proc.stdin.write("plot 'saida.txt' with lines\n")
 proc.stdin.write("pause mouse\n")
+stdscr.addstr (8,0, " Clique na figura para fecha-la ",curses.A_BOLD)
+stdscr.addstr (25,25, " ")
+stdscr.refresh()
 
 proc.stdin.write("set terminal latex\n")
+proc.stdin.write("unset key\n")
 proc.stdin.write("set output \"graph.tex\" \n")
 proc.stdin.write("plot 'saida.txt' with lines\n")
 proc.stdin.write("quit\n")
+proc.wait()
+#proc.terminate()
 os.system('cp graph.tex ./relatorio/')
+
+stdscr.addstr (9,0, "\n\nGráfico gerado. \nSaindo\n")
+stdscr.refresh()
+
+curses.nocbreak(); stdscr.keypad(0); curses.echo()
+curses.endwin()		
+
+print "Programa concluído com sucesso"
+
